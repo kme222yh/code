415cf4681f2f31ee5cfadc191cae9f9158fec9ea
kme222yh <49933526+kme222yh@users.noreply.github.com>
2019-06-03 10:55:52 +0900

    add_template

---
+++ template/js/base.js
@@ -0,0 +1,10 @@
+// 読み込み時、DOMの初期化処理など
+
+onload = function(){
+    
+    // smooth-scroll有効化
+    const scroll = new SmoothScroll('a[href*="#"]');
+    
+    
+};
+// 色々
---
+++ template/js/js/onload.js
@@ -0,0 +1,9 @@
+// 読み込み時、DOMの初期化処理など
+
+onload = function(){
+    
+    // smooth-scroll有効化
+    const scroll = new SmoothScroll('a[href*="#"]');
+    
+    
+};
